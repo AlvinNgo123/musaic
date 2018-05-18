fec94ed000eabc0c024503d13b63b552b36f9450
Hunter Lai <1hunterlai@gmail.com>
2018-05-18 02:03:40 -0700

    commented search

--- public/js/search.js
+++ public/js/search.js
@@ -10,40 +10,60 @@ var templateSourceSingle = document.getElementById('results-template-single').in
 var access_token = 
 "BQDaeSY1cr2phwut4daiWHjVuj-vp_uPlpzdATmFs-vLg_0lXX2a_xytXZsUK95YqVvvDYJXLwpI9DYJzcAg8M3DkhA1iq_UVyzTT5xYe8uvoHudIQYpvBjyOFPH5OmYmTMQjuqYlmqj_8UgC_2aTyZ6JT6ObepxtcTHWhslotzJO6eIMIJoXS2x4dRWPgfF4lSvkWtb-omLGYbZyqgqsXbZ3BCBLwGRgGi-5wUL4EZP8n3RaUlbkPb1CwpY2jp3G2R_I6gK0Q";
 
+
+
+
+//TAKES THE USER'S INPUT AND SENDS IT AS A QUERY TO SPOTIFY TO SEARCH FOR TRACKS WITH THAT NAME
 var searchTracks = function (query) {
     console.log("running searchTracks");	
+
+
+    	//call to spotify
 	    $.ajax({
 	        url: 'https://api.spotify.com/v1/search',
 	        headers: {
 	            'Authorization': 'Bearer ' + access_token
 	        },
 	        data: {
+	        	//user's input
 	            q: query,
 	            type: 'track'
 	        },
 	        success: function (response) {
 	           
+	           //uses the handlebars template to show each song on the selectSong page based on the response received
 	            resultsPlaceholderSingles.innerHTML = templateSingle(response);
 	            console.log("resultsPlaceholderSingles.innerHTML = "+resultsPlaceholderSingles.innerHTML);
 	        }
     });
 };
 
+
+
+//WHEN CLICKING ON THE ALBUM COVER THE PREVIEW WILL PLAY
 	resultsSingle.addEventListener('click', function (e) {
+
+		//the song that was clicked on
 	    var target = e.target;
+
+
 	    if (target !== null && target.classList.contains('cover')) {
-	    console.log("printing target ID: "+target.getAttribute('data-track-id'));
-	      console.log("printing target URL: "+target.getAttribute('data-prev-url'));
-	      console.log("printing track name: "+target.getAttribute('trackName'));
-	      var id = target.getAttribute('data-track-id');
-		console.log("printing artist: "+target.getAttribute('artist'));
+	  		console.log("printing target ID: "+target.getAttribute('data-track-id'));
+	     	console.log("printing target URL: "+target.getAttribute('data-prev-url'));
+	      	console.log("printing track name: "+target.getAttribute('trackName'));
+	    
 
-		console.log("printing album: "+target.getAttribute('album'));
+	      	//id of the song that was clicked on
+	     	 var id = target.getAttribute('data-track-id');
+
+			console.log("printing artist: "+target.getAttribute('artist'));
+			console.log("printing album: "+target.getAttribute('album'));
 
 
+			//Some songs don't have preview, so if it does not it will display a message
 	      if(!(target.getAttribute('data-prev-url'))){
-	      	console.log("preview not found");
-	      	document.getElementById(id).style.display = "block";
+	      		console.log("preview not found");
+	      		document.getElementById(id).style.display = "block";
 	      }else{
 	      	// document.getElementById('notPlayingMsg').setAttribute('display', 'none');
 	      		document.getElementById(id).style.display = "none";
@@ -51,7 +71,7 @@ var searchTracks = function (query) {
 
 
 
-	    
+	    	
 	        if (target.classList.contains(playingCssClass)) {
 	            audioObject.pause();
 	        } else {
@@ -59,9 +79,12 @@ var searchTracks = function (query) {
 	                audioObject.pause();
 	            }
 
+	            //create new audio object based on preview url
 	            audioObject = new Audio(target.getAttribute('data-prev-url'));
 	            audioObject.play();
 	            target.classList.add(playingCssClass);
+
+
 	            audioObject.addEventListener('ended', function () {
 	                target.classList.remove(playingCssClass);
 	            });
@@ -74,6 +97,8 @@ var searchTracks = function (query) {
 
 	
 
+//when the user presses search, it will run searchTracks which will find and populate the page with
+//search results
 document.getElementById('search-form-single').addEventListener('submit', function (e) {
     e.preventDefault();
     searchTracks(document.getElementById('query-single').value);
