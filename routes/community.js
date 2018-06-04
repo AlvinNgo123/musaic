62bcd82db6cd78e710954fe240a498e1b4423f7d
Nathan-Mansur <nathanjmansur@gmail.com>
2018-06-04 16:05:39 -0700

    Display user name in community

--- routes/community.js
+++ routes/community.js
@@ -1,3 +1,8 @@
 exports.view = function(req, res){
-  res.render('community');
+  res.render('community', {
+  	sess_id: req.session.id,
+    sess_access_token: req.session.access_token,
+    sess_refresh_token: req.session.refresh_token,
+    sess_display_name: req.session.display_name
+  });
 };
