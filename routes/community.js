5e901a7e110919cb23a1b5189344298fccaa24b4
Nathan-Mansur <nathanjmansur@gmail.com>
2018-06-06 01:39:16 -0700

    Finally saw the global userid

--- routes/community.js
+++ routes/community.js
@@ -1,9 +1,8 @@
 exports.view = function(req, res){
   res.render('community', {
-  	sess_id: req.session.id,
     sess_access_token: req.session.access_token,
     sess_refresh_token: req.session.refresh_token,
-    sess_image: req.session.image,
-    sess_display_name: req.session.display_name
+    sess_display_name: req.session.display_name,
+    sess_user_id: req.session.user_id
   });
 };
